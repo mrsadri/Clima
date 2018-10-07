01e68c8648b8a2d288f3524ad70343bd44c0a3b9
Masih Sadri <sadri.masih@gmail.com>
2018-10-07 18:24:51 +0330

    Time in Masih world is implemented.

--- Clima/WeatherDataModel.swift
+++ Clima/WeatherDataModel.swift
@@ -7,7 +7,7 @@
 import UIKit
 
 class WeatherDataModel {
-
+    
     //Moodel variables
     var tempratureDM        : Double    = 0
     var conditionDM         : Int       = 0
@@ -39,22 +39,49 @@ class WeatherDataModel {
             return  secondsToDuration(totalSeconds: dayLentghInSeconds)
         }
     }
-
+    
     var timeInMasihWorld : (hour:Int,minute: Int,second: Int){
         get{
-            return (11,59,59)
+            var returner : (hour:Int,minute:Int,second:Int) = (6,0,0)
+            let date = Date()
+            if (Int(date.timeIntervalSince1970) > sunriseTimeInterval && Int(date.timeIntervalSince1970) < sunsetTimeInterval){
+                let distanceFromSunrise = secondsToDuration(totalSeconds: Int((date.timeIntervalSince1970 - Double(sunriseTimeInterval)) / extendRate))
+                returner.hour += distanceFromSunrise.hour
+                returner.minute += distanceFromSunrise.minute
+                returner.second += distanceFromSunrise.second
+            } else if (Int(date.timeIntervalSince1970) > sunsetTimeInterval){
+                let distanceFromSunset = secondsToDuration(totalSeconds: Int((date.timeIntervalSince1970 - Double(sunsetTimeInterval)) * extendRate))
+                returner = (18,0,0)
+                returner.hour += (distanceFromSunset.hour > 6 ? distanceFromSunset.hour-24 : distanceFromSunset.hour)
+                returner.minute += distanceFromSunset.minute
+                returner.second += distanceFromSunset.second
+            } else if (Int(date.timeIntervalSince1970) < sunriseTimeInterval){
+                let distanceToSunrise = secondsToDuration(totalSeconds: Int((Double(sunriseTimeInterval) - (date.timeIntervalSince1970 )) * extendRate))
+                returner = (5,59,59)
+                returner.hour -= (distanceToSunrise.hour > 5 ? distanceToSunrise.hour+24 : distanceToSunrise.hour)
+                returner.minute -= distanceToSunrise.minute
+                returner.second -= distanceToSunrise.second
+            }
+            return returner
         }
     }
     
     var extendRate:Double{
         get{
-            return 43200/Double(dayLentghInSeconds)
+            return Double(dayLentghInSeconds)/43200
         }
     }
     
     func secondsToDuration(totalSeconds:Int) -> (hour:Int,minute: Int,second: Int){
         var returner : (hour:Int,minute: Int,second: Int) = (0,0,0)
-        returner = (totalSeconds/3600, (totalSeconds % 3600)/60 , (totalSeconds / 3600)%60 )
+        returner = (totalSeconds/3600, (totalSeconds % 3600)/60 , (totalSeconds % 3600)%60 )
+        return returner
+    }
+    
+    func addTwoTimeTouple (first:(Int,Int,Int),second:(Int,Int,Int))-> (Int,Int,Int){
+        var returner :(Int,Int,Int) = (0,0,0)
+        returner = (first.0 + second.0 > 23 ? first.0 + second.0 - 24 : first.0 + second.0
+            , first.1 + second.1, first.2 + second.2)
         return returner
     }
     
@@ -77,44 +104,44 @@ class WeatherDataModel {
     
     func updateWeatherIcon(condition: Int) -> String {
         
-    switch (condition) {
-    
+        switch (condition) {
+            
         case 0...300 :
             return "tstorm1"
-        
+            
         case 301...500 :
             return "light_rain"
-        
+            
         case 501...600 :
             return "shower3"
-        
+            
         case 601...700 :
             return "snow4"
-        
+            
         case 701...771 :
             return "fog"
-        
+            
         case 772...799 :
             return "tstorm3"
-        
+            
         case 800 :
             return "sunny"
-        
+            
         case 801...804 :
             return "cloudy2"
-        
+            
         case 900...903, 905...1000  :
             return "tstorm3"
-        
+            
         case 903 :
             return "snow5"
-        
+            
         case 904 :
             return "sunny"
-        
+            
         default :
             return "dunno"
         }
-
+        
     }
 }
